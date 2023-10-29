c6b3a90cfe99a58860214c0dc14f3825b2b21814
Daniele Carpentiero <carpentierodaniele@gmail.com>
2023-10-29 09:54:20 +0100

    feat: Monster vs Hero do-while

---
+++ CsharpProjects/TestProjects/Program.cs
@@ -0,0 +1,26 @@
+﻿int monster = 10;
+int hero = 10;
+Random value = new Random();
+
+do
+{
+    int attack = value.Next(1, 11);
+    monster -= attack;
+    Console.WriteLine($"Monster was damaged and lost {attack} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    attack = value.Next(1, 11);
+    hero -= attack;
+    Console.WriteLine($"Hero was damaged and lost {attack} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+if (hero < 0)
+{
+    Console.WriteLine("Monster wins!");
+}
+else if (monster < 0)
+{
+    Console.WriteLine("Hero wins!");
+}
